66a366d5280ea4cc057fc45f1318c86bac6ac337
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-03-05 12:10:28 -0400

    Add finally

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,8 @@ public class Main {
         }catch(NumberFormatException | ExceptionValue exception){
             System.out.println("Error: Invalid entry! Please enter an integer and greater than 0");
         }
-
-        sc.close();
+        finally {
+            sc.close();
+        }
     }
 }
